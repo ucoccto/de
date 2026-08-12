159fbbe37bfed823fda3c8a1ed05abba66e32cdd
ucoccto <edu1.ucoc@gmail.com>
2026-08-12 14:36:30 +0900

    Feat: DAG 정의 1

--- dags/01_basics_bash.py
+++ dags/01_basics_bash.py
@@ -22,9 +22,24 @@ from airflow.operators.bash import BashOperator
 # 스케쥴 -> 시간
 from datetime import datetime, timedelta
 
+# 2-1. default_args, 편의상 바깥에서 정의, 향후 내부에서 정의
+default_args = {
+  "owner"           : "aic-de1-admin",  # DAG 소유주
+  "depends_on_past" : False,            # 과거 데이터(가동 시간 대비) 소급 처리 금지
+  "retries"         : 1,                # 작업 실패시 재시도 회수 1회 설정
+  "retry_delay"     : timedelta(minutes=5) # 작업 실패후 5분후 재시도
+  # 시나리오
+  # 작업 성공 -> 완료
+  # 작업 실패 -> 5분 대기 -> 1회 재시도 -> 성공 -> 완료
+  # 작업 실패 -> 5분 대기 -> 1회 재시도 -> 실패 -> 완료(실패)
+  #            향후 작업이 재개되도(다음 스케줄에 의해)-> 누락된 과거 데이터 소급 X(백필 X)
+}
+
 # 2. DAG 정의 -> DAG 세션이 오픈된다 의미
 with DAG(
-  dag_id = "01_basics_bash" # DAG간 구분하는 용도
+  dag_id      = "01_basics_bash", # DAG간 구분하는 용도
+  description = "DE 업무중 배치 파이프라인 구성중 데이터 프로세싱 오케스트레이션 담당 airlfow의 DAG 작성 기본형", # DAG 설명
+  default_args= default_args
 ) as dag:  
   # 3. Operator 정의
 
