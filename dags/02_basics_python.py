28a7d75a774b1773291747847083d3fc8e9ed3f8
ucoccto <edu1.ucoc@gmail.com>
2026-08-12 15:40:42 +0900

    Feat: 메타 정보 추가

--- dags/02_basics_python.py
+++ dags/02_basics_python.py
@@ -17,9 +17,18 @@ def _extract_cb(**kwargs):
     - context : airflow가 주입한 정보(task 정보, 시간, ... 메타 정보). task 전달데이터    
   '''
   # 1. 컨텍스트(airflow 내부에 정보 저장공간)에서 특정 정보 추출, 키는 정의 되어 있음
-  ti = kwargs['ti']
+  #    ti : <TaskInstance: 02_basics_python.extract_task manual__2026-08-12T06:37:53.765360+00:00 [running]>
+  ti        = kwargs['ti']
+  ds        = kwargs['ds']
+  ds_nodash = kwargs['ds_nodash']
+  run_id    = kwargs['run_id']  
+
   logging.info("=== Extract 작업 ===")
   logging.info(f" ti = {ti}")
+  logging.info(f" ds = {ds}")
+  logging.info(f" ds_nodash = {ds_nodash}")
+  logging.info(f" run_id    = {run_id}")
+
 
   pass
 def _transform_cb(**kwargs):
