a14b2d86616cef0a906a7ada7241371786663357
ucoccto <edu1.ucoc@gmail.com>
2026-08-12 15:50:59 +0900

    Feat: XCOM을 통해서 데이터 획득

--- dags/02_basics_python.py
+++ dags/02_basics_python.py
@@ -39,6 +39,20 @@ def _extract_cb(**kwargs):
   return f"ds = {ds} ds_nodash = {ds_nodash} run_id    = {run_id}"
 
 def _transform_cb(**kwargs):
+  '''
+  - kwargs을 통해서 다른 task가 XCOM으로 전달한 데이터(순서상 건더 뛰어도 관계 없음)
+    - airflow conext 정보 획득 => "ti" => 전달된 데이터 접근(획득)
+  '''
+  # 1. ti 객체 획득
+  ti = kwargs["ti"]
+
+  # 2. XCOM을 통해서 데이터 획득
+  #    "extract_task" 라는  id를 가진 Task의 게시물을 가져온다
+  data = ti.xcom_pull(task_ids="extract_task")
+
+  # 3. 데이터 확인
+  logging.info("=== transform ===")
+  logging.info(f"data = {data}")
   pass
 
 
