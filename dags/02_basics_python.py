3ed3d47c63c999ebce09982a584a0df913516868
ucoccto <edu1.ucoc@gmail.com>
2026-08-12 15:26:37 +0900

    Feat: 오퍼레이터 및 의존성 정리

--- dags/02_basics_python.py
+++ dags/02_basics_python.py
@@ -24,6 +24,10 @@ with DAG(
   catchup     = False,
   tags        = ['python', 'xcom']
 ) as dag: 
+  # `ET`L
   # 3. Operator 정의 
+  extract_task   = PythonOperator()
+  transform_task = PythonOperator()
 
   # 4. 의존성 정의
+  extract_task >> transform_task
