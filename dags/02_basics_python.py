eef6b64ceb50db99c0a2e46fcd6c8b57b94d33a9
ucoccto <edu1.ucoc@gmail.com>
2026-08-12 15:20:56 +0900

    Feat: 모듈 가져오기

--- dags/02_basics_python.py
+++ dags/02_basics_python.py
@@ -1,10 +1,14 @@
 '''
 - PythonOperator 사용 패턴
 - task간 통신 -> XCom 사용 (airflow 내부 컨텍스트 공간을 접근(엑세스), 게시판) -> task 상호 대화(통신)
-- 공간의 한계 -> 공유 데이터는 raw 데이터가 아닌 raw 데이터에 점근 가능한 정보/작은 규모 raw 가능
+- 공간의 한계 -> 공유 데이터는 raw 데이터가 아닌 raw 데이터에 접근 가능한 정보/작은 규모 raw 가능
 '''
 
 # 1. 모듈 가져오기
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import logging # 레벨별 로그 출력 (에러, 경고, 정보, 디버깅,..)
 
 # 2. DAG 정의
 
