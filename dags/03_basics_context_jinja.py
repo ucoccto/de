01acffb3dee31107b2c716a4c78885fc135228d0
ucoccto <edu1.ucoc@gmail.com>
2026-08-13 09:37:06 +0900

    Feat: Dag 정의

--- dags/03_basics_context_jinja.py
+++ dags/03_basics_context_jinja.py
@@ -13,6 +13,21 @@ import pendulum
 KST = pendulum.timezone("Asia/Seoul")
 
 # 3. DAG
+with DAG(
+  dag_id      = "03_basics_context_jinja",
+  description = "macro을 이용하여 context 접근, jinja를 통해 표현",
+  default_args= {
+    "owner"           : "aic-de1-admin",    
+    "retries"         : 1,
+    "retry_delay"     : timedelta(minutes=1)
+  },
+  # 매일 오전 9시 00분에 스케줄 작동
+  schedule_interval = "0 9 * * *", # cron 방식으로 표기 (분, 시, 일, 월, 주)
+  # 수행 시작 시간 서울 시간대 타임존 조정
+  start_date  = pendulum.datetime( 2026,6,29, tz=KST ),
+  catchup     = False,
+  tags        = ['macro', 'context', 'jinja']
+) as dag:
 
   # 4. 오퍼레이터
 
