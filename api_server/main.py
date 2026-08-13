39895094d0e55a5be0106c3aedb44acce5c83e64
ucoccto <edu1.ucoc@gmail.com>
2026-08-13 15:39:54 +0900

    Feat: 요청/응답 구조 정의

--- api_server/main.py
+++ api_server/main.py
@@ -9,7 +9,20 @@ from typing import List         # 요청/응답 데이터 구성시 구조 정�
 import random                   # 신용평가시 활용
 
 # 2. FastAPI 객체 생성
+app = FastAPI()
 
 # 3. 요청/응답 구조 정의 => class
+class ReqData(BaseModel):
+  # 컬럼 나열
+  user_id:str   # 사용자 아이디
+  income:int    # 소득
+  loan_amt:int  # 현재 총 대출액
+
+class ResData(BaseModel):
+  # 컬럼 나열
+  user_id:str         # 사용자 아이디
+  credit_score:int    # 0점 ~ 1000점
+  grade:str           # S급, A급, B급, C급,...
+
 
 # 4. 라우팅 : url, 처리함수 매핑 정의
