639ac01437076004c40972e5b4c9deb661e886fe
ucoccto <edu1.ucoc@gmail.com>
2026-08-13 15:50:14 +0900

    Feat: 신용평가 백엔드 코드 구성 완료

--- api_server/main.py
+++ api_server/main.py
@@ -26,3 +26,31 @@ class ResData(BaseModel):
 
 
 # 4. 라우팅 : url, 처리함수 매핑 정의
+@app.get("/")
+def home():
+  return {"status":"AI 신용평가 서비스 API"}
+
+@app.post("/predict", response_model=List[ResData]) # 응답 구조 정의
+def predict( users:List[ReqData]):  # 요청 구조 정의
+  # 고객 1명씩 신용평가하여 => 응답구조 작성후 => 응답
+  results = list()
+  for user in users: # 고객 1명씩 추출
+    '''
+      가상 공식
+      사전반영식 = (소득//1000) * 10
+      credit_score = min( 난수(300, 600) + 사전반영식, 990 )
+      grade    = credit_score이 800 이상이면 A, 600 이상이면 B, 나머지 C
+    '''
+    # 고객 1명 평가
+    사전반영식 = (user.income//1000) * 10
+    credit_score = min( random.randint(300, 600) + 사전반영식, 990 )
+    grade    = "A" if credit_score >= 800 else "B" if credit_score >= 600 else "C"
+    # 평가한 고객 정보 담기
+    results.append({
+      "user_id"       : user.user_id,
+      "credit_score"  : credit_score,
+      "grade"         : grade
+    })
+
+  # 응답
+  return results
