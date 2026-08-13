f3220985b697025ce95ca7f66ef7f70c1f2f380f
ucoccto <edu1.ucoc@gmail.com>
2026-08-13 13:51:45 +0900

    Feat: Mysql 접속

--- dags/05_mysql_etl.py
+++ dags/05_mysql_etl.py
@@ -85,6 +85,21 @@ def _transform(**kwargs):
 
 
 def _load(**kwargs):
+  # 1. csv 경로 획득
+  ti             = kwargs["ti"]
+  csv_file_path  = ti.xcom_pull(task_ids="transform")
+  logging.info( f'전달한 데이터 파일 경로 {csv_file_path}'  )
+  
+  # 2. csv -> df 로드
+  df = pd.read_csv( csv_file_path )
+
+  # 3. mysql 연결 -> 데이터 삽입
+  hooks = MySqlHook(mysql_conn_id="mysql_default")
+  try:
+    with hooks.get_conn() as conn:
+      pass
+  except Exception as e:
+    logging.error(f'sql  에러 { e }')
   pass
 
 # DAG 정의
