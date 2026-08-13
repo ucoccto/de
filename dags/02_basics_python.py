7b5c9245b69f45bb01e8bed740d05f4a565b0d4c
ucoccto <edu1.ucoc@gmail.com>
2026-08-13 09:25:28 +0900

    Feat: 서울 시간대 작업 시간 보정

--- dags/02_basics_python.py
+++ dags/02_basics_python.py
@@ -29,7 +29,9 @@ def _extract_cb(**kwargs):
   # 시간 보정
   logical_date     = kwargs["logical_date"]
   logical_date_kst = logical_date.in_timezone(KST)
-  
+  # 차후 s3등에 적제할때 파티션시 활용함 yyyy, mm, dd 끊어서 사용시 활용
+  ds_kst           = logical_date_kst.format("YYYY-MM-DD")
+  ds_nodash_kst    = logical_date_kst.format("YYYYMMDD")
 
   logging.info("=== Extract 작업 ===")
   logging.info(f" ti = {ti}")
@@ -40,6 +42,8 @@ def _extract_cb(**kwargs):
   '''
   logging.info(f" ds = {ds}")
   logging.info(f" ds_nodash = {ds_nodash}")
+  logging.info(f" ds_kst = {ds_kst}")
+  logging.info(f" ds_nodash_kst = {ds_nodash_kst}")
   logging.info(f" run_id    = {run_id}")
 
   # 정보 전달 -> XCOM 게시판에 본 task가 글을 작성하는것
