e5c5bb4b119db728afa172891de59793a06f9348
ucoccto <edu1.ucoc@gmail.com>
2026-08-14 14:10:52 +0900

    Feat: 환경변수 적용

--- dags/08_aws_s3_basic.py
+++ dags/08_aws_s3_basic.py
@@ -13,8 +13,18 @@
 # 1. 모듈 가져오기
 from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+import logging
+import pendulum
 
-# 2. 환경변수(전역변수)
+# 2. 환경변수(전역변수) -> .env, airflow 환경변수
+BUCKET_NAME      = "de-ai-25-infra-s3-bk-827913617635"
+UPLOAD_FILE_NAME = "costs.csv" # 각자 다를수 있음
+LOCAL_PATH       = f"/opt/airflow/dags/data/{UPLOAD_FILE_NAME}" # 컨테이너에서 업로드 경로
+
 # 3. DAG 정의
   # 4. task 
   # 5. 의존성
