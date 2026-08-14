19c94c8c84ca96ad1e083050af69ee136a539b72
ucoccto <edu1.ucoc@gmail.com>
2026-08-14 13:28:13 +0900

    Feat: 버킷 소유자 권리등 설정

--- infra/s3.tf
+++ infra/s3.tf
@@ -9,3 +9,35 @@ locals {
   # 최종버킷명 : de-ai-25-infra-s3-bk-827913617635
   airflow_bucket_name = "${var.project_name}-s3-bk-${data.aws_caller_identity.current.account_id}"
 }
+
+# 버킷 생성
+resource "aws_s3_bucket" "airflow_data" {
+  # 버킷명
+  bucket = local.airflow_bucket_name
+  # 버킷을 삭제할때
+  # false : 버킷 내부에 Object 남아 있다면, terraform destroy 수행시 버킷 삭제를 막음 -> 에러남
+  # true  : 버킷 내부에 Object 까지 모두 제거 => 버킷 삭제
+  force_destroy = var.s3_force_destroy
+
+  # 공용 태그
+  tags = merge(
+    local.common_tags,
+    {
+      Name = local.airflow_bucket_name
+    }
+  )
+}
+
+# S3 Object Ownership(객체 소유권)
+resource "aws_s3_bucket_ownership_controls" "airflow_data" {
+  bucket = local.airflow_bucket_name
+
+  rule {
+    # BucketOwnerEnforced
+    # - ACL 비활성화됨(권장), ACL 기능 사용 x
+    # - 버킷 소유자가 버킷 내부의 객체의 소유권을 가진다
+    # - 접근 제어 IAM Policy / Bucket Policy 중심으로 관리한다 
+    #   -> ACL 방식 x, 계정 소유자(IAM) 권한으로 관리
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
