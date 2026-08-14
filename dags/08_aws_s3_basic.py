cd8f592a7e43fd416ddf2344d5131d8e687b556b
ucoccto <edu1.ucoc@gmail.com>
2026-08-14 13:48:33 +0900

    Feat: s3 기본 처리 dag 구성

---
+++ dags/08_aws_s3_basic.py
@@ -0,0 +1,19 @@
+'''
+- airflow aws를 엑세스 -> 오퍼레이터등 도구 제공 -> 패키지 설치
+- docker-compose.yaml
+  - apache-airflow-providers-amazon 추가
+  - _PIP_ADDITIONAL_REQUIREMENTS: ...viders-mysql apache-airflow-providers-amazon
+
+- docker compose down
+- docker compose up -d
+
+- 로컬 설치 : pip install apache-airflow-providers-amazon
+- 원격 PC에서 AWS S3의 특정 버킷(보인 소유의)에 간단하게 데이터 업로드 테스트 DAG
+'''
+# 1. 모듈 가져오기
+# 2. 환경변수(전역변수)
+# 3. DAG 정의
+  # 4. task 
+  # 5. 의존성
+
+
