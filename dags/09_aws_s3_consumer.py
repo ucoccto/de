39410c17374fe58f7d7ab9b40d1348a96263ad9a
ucoccto <edu1.ucoc@gmail.com>
2026-08-14 15:24:58 +0900

    Feat: S3KeySensor 구성 완료

--- dags/09_aws_s3_consumer.py
+++ dags/09_aws_s3_consumer.py
@@ -16,9 +16,46 @@ import logging
 import pendulum
 
 # 2. 환경변수
+BUCKET_NAME      = "de-ai-25-infra-s3-bk-827913617635"
+UPLOAD_FILE_NAME = "sensor_data.csv"              # 현재시간등 인식/구분 정보 누락
+S3_KEY           = f'airflow/{UPLOAD_FILE_NAME}'  # s3상 위치 조정
 
 # 3. DAG
+with DAG(  
+  dag_id      = "09_aws_s3_consumer",
+  description = "s3 특정 버킷내 특정 위치 감시, 감지된 이후 처리",
+  default_args= {
+    "owner"           : "aic-de1-admin",    
+    "retries"         : 1,
+    "retry_delay"     : timedelta(minutes=1)
+  },
+  schedule_interval = '@daily', # 최소한의 스케줄 필요 -> 구동중이여야 작도 가능
+  start_date  = pendulum.datetime( 2026,6,29, tz=pendulum.timezone("Asia/Seoul") ),
+  catchup     = False,
+  tags        = ['aws', 's3', 'consumer']
+) as dag:  
   
-  # 4. task
+  # 4. task 
+  # 감시자, 센서
+  task_wait_for_trigger = S3KeySensor(
+    task_id = "wait_for_trigger",
+    # 감시 대상 설저
+    bucket_name = BUCKET_NAME,
+    bucket_key  = S3_KEY,        # 디렉토리 x, 디렉토리내 키(향후 키가 전달되야 일반화될수 있을듯)
+    aws_conn_id = "aws_default", # 접속 정보
+    # 감시 방법 -> 지속적(주기적-인터벌) 감시 -> 서비스 방식, 구성에 따라 설계 필요
+    mode          = "reschedule",# 감시 대기중에 자원 반납
+    poke_interval = 10,          # 10초 간격으로 체크
+    timeout       = 60*10        # 10분 넘게 서비스 가동후 감지가 않되면 종료
+    
+  )
+  task_reading_data     = PythonOperator(
+    task_id = "reading_data"
+  )
+  # 삭제는 컨셉이고, 실제는 백업 처리
+  task_delete_data      = S3DeleteObjectsOperator(
+    task_id = "delete_data"
+  )
 
   # 5. 의존성
+  task_wait_for_trigger >> task_reading_data >> task_delete_data
