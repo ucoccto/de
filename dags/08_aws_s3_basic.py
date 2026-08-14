b87b07caa48ce0f74b9579c49c782254368f295e
ucoccto <edu1.ucoc@gmail.com>
2026-08-14 13:55:08 +0900

    Hotfix: 테라폼 수정

--- dags/08_aws_s3_basic.py
+++ dags/08_aws_s3_basic.py
@@ -11,6 +11,9 @@
 - 원격 PC에서 AWS S3의 특정 버킷(보인 소유의)에 간단하게 데이터 업로드 테스트 DAG
 '''
 # 1. 모듈 가져오기
+from airflow.providers.amazon.aws.transfers.local_to_s3 import LocalFilesystemToS3Operator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+
 # 2. 환경변수(전역변수)
 # 3. DAG 정의
   # 4. task 
