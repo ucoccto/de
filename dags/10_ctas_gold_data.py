89def6acdc3989a2a4cc6e81f714f57b3bea4890
ucoccto <edu1.ucoc@gmail.com>
2026-08-27 11:08:55 +0900

    Feat: 환경변수를 통해서 각종 경로, 이름 설정

--- dags/10_ctas_gold_data.py
+++ dags/10_ctas_gold_data.py
@@ -7,6 +7,23 @@ from airflow.providers.amazon.aws.operators.athena import AthenaOperator
 from airflow.providers.amazon.aws.operators.s3 import S3DeleteObjectsOperator
 
 # 2. 환경변수
+AWS_CONN_ID = "aws_default"
+# 버킷
+BUCKET_NAME = "de-ai-25-loggen-s3-bk-827913617635"
+# 디비명
+DATABASE_NAME = "de_ai_25_loggen_silver_glue_db"
+# 테이블명
+SILVER_TABLE_NAME = "silver_logs_tbl"
+
+# 1회성 테이블(24시간 유지 -> 다음번 batch 작업시 삭제, 신규 생성 테이블), 운영 사용 테이블 겹치면 x
+GOLD_TABLE_NAME = "gold_daily_report_ctas_tbl"
+
+# Athena SQL 실행 결과 저장 => [v]직접 지정 or 작업 그룹 지정 => 저장되는 위치가 결정  
+QUERY_RESULT_S3 = f"s3://{BUCKET_NAME}/athena/dags/"
+
+# CTAS가 실제로 참조하는 데이터 저장위치
+GOLD_PREFIX = "gold/daily_report_ctas/"
+GOLD_LOCATION = f"s3://{BUCKET_NAME}/{GOLD_PREFIX}"
 
 # 3. DAG 정의
 
