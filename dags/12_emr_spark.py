ad85f2ec75f1cf71bc5e7b6c9991d769bccb8d9b
edu2ucoc <edu2.ucoc@gmail.com>
2026-09-16 14:47:05 +0900

    feat: 인프라 구성 job flow 코멘트 완료

--- dags/12_emr_spark.py
+++ dags/12_emr_spark.py
@@ -12,38 +12,62 @@ BUCKET_NAME = 'de-ai-25-loggen-s3-bk-827913617635'
 SPARK_SCRIPT_PATH = f"s3://{BUCKET_NAME}/spark/script/spark_etl.py"
 EMR_LOG_URI = f"s3://{BUCKET_NAME}/spark/emr_logs/"
 
-# 3. 인프라 설정 dict
+# 3. 인프라 설정 dict 형태 : json 구조로 구성
+#    장점 : 스케줄, 프로그램과 같이 진행 => 관리 용이, 필요할때만 사용할때 유용
 JOB_FLOW_OVERRIDES = {
-    "Name": "Airflow-Automated-EMR-Cluster-de30",
+    # 클러스터 구분용, 리소스 id는 내부적으로 반환됨
+    "Name": "Airflow-Automated-EMR-Cluster-de25",
+    # EMR 소프트웨어의 버전 지정 => EC2 + hadoop + spark + emr 환경 구성
     "ReleaseLabel": "emr-6.10.0",
+    # Hadoop, Spark 설치하도록 지정
+    # Hadoop 환경은 스파크 구동에 기반 역활 담당
     "Applications": [
         {"Name": "Hadoop"},
         {"Name": "Spark"},
     ],
+    # EC2 기반 EMR 구성
+    # 인스턴스 생성
     "Instances": {
+        # 테라폼 -> VPC 구성 => 동적 세팅
+        # 약식 구성 : 기본 VPC의 a존의 서브넷 고정
         "Ec2SubnetId": "subnet-0928223142a64ef05",
         "InstanceGroups": [
+            # 테스트 하면서 데이터량에 맞춰 수치/사양 조정해야 함 -> 개발자가 직접 구성, 운영 마스터가 알아서함
+            # 마스터, 워커 노드가 운용될 eC2 인스턴스(머신)을 구성
+            # EMR 클러스터 관리용(스파크 작업 관리, 리소스 관리, 워커노드 관리) 노드
+            # 데이터 증가 => 현재 구성상 관리 힘들 => 용량 증가하라 등 처리
             {
+                # 마스터
                 "Name": "Master node",
+                # 스팟 : 비용 저렴, EC2 용량대비 할인 가격을 사용. 단, 시스템에 의해 중단 가능성 존재함
+                # Master는 안정성이 중요해서
                 "Market": "SPOT",
+                #"Market": "ON_DEMAND",  # <- 이 구성이 더 나음
                 "InstanceRole": "MASTER",
                 "InstanceType": "m5.xlarge",
                 "InstanceCount": 1,
             },
             {
+                # 워커
                 "Name": "Core nodes",
                 "Market": "SPOT",
                 "InstanceRole": "CORE",
                 "InstanceType": "m5.xlarge",
+                # 인스턴스 2개 지정 -> 작업량에 따라 관리
                 "InstanceCount": 2,
             },
         ],
+        # 스파크 작업이 끝났다고 스스로 종료 x -> EmrTerminateJobFlowOperator에 의해 명시적 종료되게 True 적용
         "KeepJobFlowAliveWhenNoSteps": True,
+        # 종료 보호 장치 해제, True이면 EmrTerminateJobFlowOperator와 충돌 날수 있음
         "TerminationProtected": False,
     },
-    "JobFlowRole": "EMR_EC2_DefaultRole",
-    "ServiceRole": "EMR_DefaultRole",
+    # 최소 권한 부여했다
+    "JobFlowRole": "EMR_EC2_DefaultRole", # EC2 인스턴스가 사용하는 IAM ROLE
+    "ServiceRole": "EMR_DefaultRole",     # EMR 서비스에 대한 기본 IAM ROLE
+    # EMR 로그 저장 위치 지2정
     "LogUri": EMR_LOG_URI,
+    # 클러스터의 가시성 설정
     "VisibleToAllUsers": True,
 }
 SPARK_SUBMITS = [
