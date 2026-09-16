63818dcefbfcf97bc49a968338a699c754209670
edu2ucoc <edu2.ucoc@gmail.com>
2026-09-16 13:46:46 +0900

    Feat: 구조1

--- dags/12_emr_spark.py
+++ dags/12_emr_spark.py
@@ -1,4 +1,7 @@
 # 1. 모듈 가져오기
+from datetime import timedelta
+import pendulum
+from airflow import DAG
 
 # 2. 환경변수
 
@@ -7,7 +10,20 @@
 # 4. 콜백함수
 
 # 5. dag 정의
-
+with DAG(  
+  dag_id      = "12_EMR_SPARK",
+  description = "대용량 데이터를 분산환경에서 ETL하기 위해 스파크 사용",
+  default_args= {
+    "owner"           : "aic-de1-admin",    
+    "retries"         : 1,
+    "retry_delay"     : timedelta(minutes=1)
+  },
+  schedule_interval = "daily",
+  start_date  = pendulum.datetime( 2026,6,29, tz=pendulum.timezone("Asia/Seoul") ),
+  catchup     = False,
+  tags        = ['aws', 'spark', 'emr']
+) as dag:
     # 6. task 구성
 
     # 7. 의존성
+    pass
